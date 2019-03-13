9140dc2375dbffa128275a65168a12cbaccaf76b
Shankar Kulumani <shanks.k@gmail.com>
2019-03-12 21:31:35 -0400

    Debugging the ray casting

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -8,17 +8,20 @@
 
 void render(const Sphere& sphere) {
     // size of image
-    const int width    = 10;
-    const int height   = 10;
+    const int width    = 800;
+    const int height   = 800;
 
     // define camera FOV
     const float fov_h = M_PI/3.0;
     const float fov_w = M_PI/3.0;
-
+    
+    // this defines the camera frame (and images on the image plane)
+    // TODO Rotate these vectors camera to inertial frame
     const Vec3f view_axis(0, 0, 1);
-    const Vec3f up_axis(0, -1, 0);
+    const Vec3f up_axis(0, 1, 0);
     const Vec3f right_axis(1, 0, 0);
-    const float dist(1);
+
+    const float dist(10);
     const Vec3f cam_axis(0, 0, dist);
 
     // data for the image RGB 
@@ -34,10 +37,10 @@ void render(const Sphere& sphere) {
             float dir_x = (ii + 0.5) - width/2;
             float dir_y = (jj + 0.5) - height/2;
             float dir_z = height/(2.0*std::tan(fov_h/2));
+            Vec3f ray_vec(dir_x, dir_y, dir_z);
 
             /* framebuffer[ii+jj*width] = Vec3f(jj/float(height),ii/float(width), 0); */
-            framebuffer[ii+jj*width] = cast_ray(Vec3f(0, 0, 0), Vec3f(dir_x, dir_y, dir_z).normalize(), sphere);
-            /* framebuffer[ii+jj*width] = cast_ray(Vec3f(0, 0, 0), ray_vec.normalize(), sphere); */
+            framebuffer[ii+jj*width] = cast_ray(Vec3f(0, 0, 0), ray_vec.normalize(), sphere);
 
         }
     }
@@ -48,6 +51,7 @@ void render(const Sphere& sphere) {
     // data array type for STB
 
     std::uint8_t image[width * height * 3];
+
     #pragma omp parallel for
     for (size_t ii = 0; ii < width * height; ii++) {
         image[ii * 3 + 0] = (int)(255 * framebuffer[ii][0]);
