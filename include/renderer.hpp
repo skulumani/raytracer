8d7a4ff2fcfb05c5dcde39f88e5f67dd4dd6bf17
Shankar Kulumani <shanks.k@gmail.com>
2019-03-15 15:59:42 -0400

    Add light

--- include/renderer.hpp
+++ include/renderer.hpp
@@ -7,9 +7,23 @@
 #include <fstream>
 #include <vector>
 
+#include <Eigen/Core>
+
 #include "geometry.hpp"
 
 void render( const Sphere& sphere );
 void render(const std::vector<Sphere>& spheres);
+
+struct Light {
+    private:
+        Eigen::Vector3f position;
+        float intensity;
+
+    public:
+        Light(const Eigen::Ref<const Eigen::Vector3f>& pos_in,
+                const float& int_in);
+        Light( void );
+
+};
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@ int main() {
     // define different materials
     Material ivory(0.4, 0.4, 0.3);
     Material red_rubber(0.3, 0.1, 0.1);
+    Material plywood(0.835, 0.796, 0.325);
+    Material teal(0.325, 0.835, 0.796);
 
     // define a sphere
     //
@@ -16,9 +18,10 @@ int main() {
     std::vector<Sphere> spheres;
     spheres.push_back(Sphere(-3,    0,   16, 2,      ivory));
     spheres.push_back(Sphere(-1.0, -1.5, 12, 2, red_rubber));
-    spheres.push_back(Sphere( 1.5, -0.5, 18, 3, red_rubber));
-    spheres.push_back(Sphere( 7,    5,   18, 4,      ivory));
+    spheres.push_back(Sphere( 1.5, -0.5, 18, 3, plywood));
+    spheres.push_back(Sphere( 7,    5,   18, 4,      teal));
 
     render(spheres); 
+
     return 0;
 }
